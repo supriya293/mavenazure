bc164bedeb3ecaf2d0acf482f167ad6da422651f
supriya293 <supriya.perar@gmail.com>
2025-04-23 10:35:44 +0530

    azure pipeline example

---
+++ src/main/java/myGroup/mavenazure/App.java
@@ -0,0 +1,13 @@
+package myGroup.mavenazure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "experiment10" );
+    }
+}
